645167121f3eeefacd97eaf1b7760564ba5e4463
tenoriobruno <bruno.tenorio1@gmail.com>
2014-04-27 16:57:58 -0300

    estacioneAki mobile com google maps

--- app/controllers/Estacionamentos.java
+++
@@ -1,5 +0,0 @@
-package controllers;
-
-public class Estacionamentos extends CRUD {
-
-}
---
+++ estacione_aki_mobile/estacioneAkiMobile/src/estacioneAki/activities/EstacioneAkiMap.java
@@ -0,0 +1,13 @@
+package estacioneAki.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class EstacioneAkiMap extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_estacione_aki_map);
+    }
+}
