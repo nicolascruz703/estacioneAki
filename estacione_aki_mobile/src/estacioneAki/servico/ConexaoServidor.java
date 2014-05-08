9553e94d8f1754be18d7a3460fae138c7c9faa4f
tenoriobruno <bruno.tenorio1@gmail.com>
2014-05-08 13:16:53 -0300

    projeto com a dependencia

---
+++ estacione_aki_mobile/src/estacioneAki/servico/ConexaoServidor.java
@@ -0,0 +1,21 @@
+package estacioneAki.servico;
+
+import estacioneAki.util.EstacionamentoList;
+
+public class ConexaoServidor {
+  	
+	public EstacionamentoList reservarVaga(String cpf, String cnpj){
+		return null;
+		
+	}
+	
+	public EstacionamentoList cancelarVaga(String cpf){
+		return null;
+	}
+	
+	public EstacionamentoList listaEstacionamentos(){
+		return null;
+	}
+
+}
+
