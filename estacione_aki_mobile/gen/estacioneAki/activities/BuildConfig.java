853ba5e4eb5eabaf6d8383a3966002227a5eae14
tenoriobruno <bruno.tenorio1@gmail.com>
2014-05-28 15:47:39 -0300

    limpando o git estacioneAki

--- estacione_aki_mobile/gen/estacioneAki/activities/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package estacioneAki.activities;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- estacione_aki_mobile/src/estacioneAki/servico/URLs.java
+++
@@ -1,9 +0,0 @@
-package estacioneAki.servico;
-
-public interface URLs {
-	
-	public String LISTA_ESTACIONAMENTOS = "http://54.207.83.104:9000/MobileInterface/listaEstacionamento";
-	public String RESERVAR_VAGA = "http://54.207.83.104:9000/mobileinterface/reservaVaga?";
-	public String CANCELAR_VAGA = "http://54.207.83.104:9000/mobileinterface/cancelaReservaVaga?";
-
-}
